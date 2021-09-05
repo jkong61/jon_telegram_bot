93b39dfedb164e73fe6dfb24cd9931989a34d9f0
Jonathan Kong <joako407@gmail.com>
2021-09-05 19:42:19 +0800

    Implement ngrok

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.bot_command import BotCommand
 from aiogram.types.chat import ChatActions
 from aiohttp import ClientSession
+from dotenv import dotenv_values
+from pyngrok import ngrok
+
+ENV = dotenv_values(".env")
+
 
 URL = 'https://my-json-server.typicode.com/typicode/demo/db'
-API_TOKEN = "TOKEN HERE"
+API_TOKEN = ENV['API_TOKEN']
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+
+async def on_startup(dp):
+  ngrok.set_auth_token(ENV['NGROK_TOKEN'])
+  public_url = ngrok.connect(8443,bind_tls=True).public_url
+  await bot.set_webhook(f"{public_url}/{API_TOKEN}")
+  await bot.set_my_commands(commands=[
+    BotCommand('start','Start Command'),
+    BotCommand('getdata','Fetch Data From Server')
+  ])
+
+async def on_shutdown(dp):
+  ngrok.kill()
 
 async def fetch(url : str, session :  ClientSession):
   async with session.get(url) as response:
@@ -37,4 +55,10 @@ async def handle_sticker(message: types.Message):
   await message.answer('Cute sticker!')
 
 if __name__ == '__main__':
-  executor.start_polling(dp, skip_updates=True)
+  executor.start_webhook(
+    dp,
+    webhook_path="/{API_TOKEN}",
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    port=8443
+  )
